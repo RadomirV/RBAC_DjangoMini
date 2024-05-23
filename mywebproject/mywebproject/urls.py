64d3955d0b162bbaba8fb566c06bfaa8d9a71506
Radomir Olennikov <rimodar2809@gmail.com>
2024-05-23 23:00:48 +0700

    delete files

--- mywebproject/mywebproject/urls.py
+++ mywebproject/mywebproject/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 URL configuration for mywebproject project.
 
@@ -29,32 +28,3 @@ urlpatterns = [
     path('notes/create/', create_note, name='create_note'),
     path('notes/', note_list, name='note_list'),
 ]   
-=======
-"""
-URL configuration for mywebproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from mywebapp.views import login_view, home_view, logout_view, registration_view
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('home/', home_view, name='home'),
-    path('registration/', registration_view, name='registration'),
-]
->>>>>>> 84cd1563f84fde80347479da1b81d5b66bc77fea
